4bd9157d3a90074a7ff38cf5c0f105fc22e7d115
grigoriev.tver <grigoriev.tver@gmail.com>
2020-02-14 23:56:30 +0300

    create actions for get and process data

--- store/index.js
+++ store/index.js
@@ -10,6 +10,8 @@ const store = () => new Vuex.Store({
     isLoading: false,
     dataList: [],
     map: null,
+    entityData: null,
+    proxy: 'https://cors-anywhere.herokuapp.com',
   },
   actions: {
     setMap({ commit }, map) {
@@ -18,6 +20,45 @@ const store = () => new Vuex.Store({
     setReady({ commit }, ready) {
       commit('SET_READY', ready)
     },
+    async getData({ state, dispatch }) {
+      let data;
+
+      try {
+        data = await this.$axios.$get(`${state.proxy}/https://covid2019-api.herokuapp.com/current?${Date.now()}`);
+
+        dispatch('processData', data)
+      } catch (e) {
+        console.log(e);
+        dispatch('getReserveData');
+      }
+    },
+    async getReserveData({ state, dispatch }) {
+      let data;
+
+      try {
+        data = await this.$axios.$get(`${state.proxy}/https://coronavirus.zone/data.json?${Date.now()}`);
+
+        dispatch('processData', data)
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    processData({ commit }, data) {
+      let processed;
+
+      if (Array.isArray(data))  {
+        processed = data.map(item => ({
+          region: item.region,
+          confirmed: item.cases,
+          deaths: item.death,
+        }));
+      } else {
+        processed = Object.keys(data)
+          .reduce((acc, curr) => (!['ts', 'dt'].includes(curr) && acc.push({ region: curr, ...data[curr] }), acc), []);
+      }
+
+      commit('SET_DATA', processed);
+    },
   },
   mutations: {
     SET_MAP(state, map) {
@@ -25,6 +66,9 @@ const store = () => new Vuex.Store({
     },
     SET_READY(state, ready) {
       state.isReadyMap = ready
+    },
+    SET_DATA(state, data) {
+      state.entityData = data
     },
   },
 });
